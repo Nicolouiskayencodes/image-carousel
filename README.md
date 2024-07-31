4d6b728242b46eccb3b5fac998f4597041ad4123
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-07-30 23:55:24 -0700

    Add left and right buttons to carousel

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-# Webpack-Template
-run these commands
-npm install --save-dev html-webpack-plugin
-npm install --save-dev webpack-dev-server
-npm install --save-dev style-loader css-loader
+# image-carousel
--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
 import './style.css';
-console.log('test');
+const back = document.querySelector('#left');
+const forward = document.querySelector('#right');
+const content = document.querySelector('#content')
+console.log((parseInt(getComputedStyle(content).left)));
+forward.addEventListener('click', function() {
+  if ((parseInt(getComputedStyle(content).left)) > -800){
+    let int = (parseInt(getComputedStyle(content).left) - 200);
+    console.log(int);
+    content.style.left = int + 'px';
+  }
+})
+back.addEventListener('click', function() {
+  if ((parseInt(getComputedStyle(content).left)) < 0){
+    let int = (parseInt(getComputedStyle(content).left) + 200);
+    console.log(int);
+    content.style.left = int + 'px';
+  }
+})
